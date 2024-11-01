a3833e2269df6ede41ea0a806880982403bc0034
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2024-11-01 17:20:26 +0100

    Editted the landing page to include headers

--- frontend/src/screens/alex_bill.jsx
+++ frontend/src/screens/alex_bill.jsx
@@ -31,7 +31,7 @@ const AlexAndBillEstate = () => {
 
     try {
       setIsLoading(true);
-      const response = await fetch("https://360-distinct-real-estate-backend.vercel.app/api/brochure", {
+      const response = await fetch("/api/brochure", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -69,7 +69,7 @@ const AlexAndBillEstate = () => {
 
     try {
       setIsLoading(true);
-      const response = await fetch("https://360-distinct-real-estate-backend.vercel.app/api/virtual-tour", {
+      const response = await fetch("/api/virtual-tour", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
