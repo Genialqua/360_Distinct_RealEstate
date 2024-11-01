4819049c24218e73b8a6bbcd41e774f9fcc58346
Frederick Aleoke-Malachi <fred.aleokemalachi@gmail.com>
2024-11-01 14:02:14 +0100

    Updated the codebase for dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import { Outlet } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
+
 const App = () => {
   return (
     <>
